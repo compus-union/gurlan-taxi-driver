7b37dcd92b1b2418f6126c23448b1de3ed7f47ea
dasturchioka <dasturchioka@gmail.com>
2023-09-13 17:22:13 +0500

    working with layouts

---
+++ src/layouts/RegisterLayout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {
+  IonPage,
+  IonContent,
+  IonButton,
+  IonCheckbox,
+  IonRouterOutlet,
+  IonText,
+} from "@ionic/vue";
+</script>
+
+<template>
+  <IonPage>
+    <IonContent class="auth-content font-bricolage">
+      <IonRouterOutlet></IonRouterOutlet>
+    </IonContent>
+  </IonPage>
+</template>
