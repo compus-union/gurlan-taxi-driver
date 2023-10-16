77f9c7fba4aa68578f1957327ef88a1cf900e57f
dasturchioka <dasturchioka@gmail.com>
2023-10-16 14:40:35 +0500

    adding stores with AI )

---
+++ src/stores/maps.ts
@@ -0,0 +1,92 @@
+import { defineStore } from "pinia";
+import { Loader } from "@googlemaps/js-api-loader";
+import { ref } from "vue";
+
+// Define a store for maps
+export const useMaps = defineStore("maps-store", () => {
+  // Initialize a loader with the API key and version
+  const loader = new Loader({
+    apiKey: `AIzaSyDKUe31BzfxFHUc4harDoHY61VSpLC3d4g`,
+    version: "weekly",
+  });
+
+  // Create a reference for the map and marker
+  const sharedMap = ref<google.maps.Map>();
+  const markers = ref([]);
+
+  // Load the Google Maps API
+  async function loadMap(id: string) {
+    
+    const { Map } = await loader.importLibrary("maps") as google.maps.MapsLibrary;
+    sharedMap.value = new Map(document.getElementById(id) as HTMLElement, {
+      center: { lat: 5, lng: 5 },
+      zoom: 20,
+      mapTypeId: "OSM",
+      mapTypeControl: false,
+      streetViewControl: false,
+      disableDefaultUI: true,
+      rotateControl: true,
+    });
+
+    if (sharedMap.value) {
+      sharedMap.value.mapTypes.set(
+        "OSM",
+        new google.maps.ImageMapType({
+          getTileUrl: function (coord, zoom) {
+            let tilesPerGlobe = 1 << zoom;
+            let x = coord.x % tilesPerGlobe;
+            if (x < 0) {
+              x = tilesPerGlobe + x;
+            }
+
+            return (
+              "https://tile.openstreetmap.org/" +
+              zoom +
+              "/" +
+              x +
+              "/" +
+              coord.y +
+              ".png"
+            );
+          },
+          tileSize: new google.maps.Size(256, 256),
+          name: "OpenStreetMap",
+          maxZoom: 18,
+        })
+      );
+
+      const newMarker = new google.maps.Marker({
+        map: sharedMap.value,
+        position: sharedMap.value?.getCenter(),
+        title: "origin-marker",
+      });
+
+      sharedMap.value.addListener("drag", () => {
+        if (sharedMap.value && sharedMap.value.getCenter()) {
+          newMarker.setPosition(sharedMap.value.getCenter());
+        }
+      });
+
+      sharedMap.value.addListener("dragend", async () => {
+        const lat = sharedMap.value?.getCenter()?.lat() as number;
+        const lng = sharedMap.value?.getCenter()?.lng() as number;
+
+        console.log(lat, lng);
+      });
+
+      markers.value?.push(newMarker as unknown as never);
+    }
+
+    return {
+      Map,
+      sharedMap,
+    };
+  }
+
+  // Return the map and marker
+  return {
+    sharedMap,
+    markers,
+    loadMap
+  };
+});
---
+++ src/stores/origin.ts
@@ -0,0 +1,83 @@
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
+import { Geolocation } from "@capacitor/geolocation";
+import router from "@/router";
+
+export const useOriginCoords = defineStore("origin-coords-store", () => {
+  const lat = ref<number>(0);
+  const lng = ref<number>(0);
+
+  const watchingCoords = ref<boolean>(true);
+
+  async function getCoordsWithNavigator(): Promise<void> {
+    try {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords }) => {
+          lat.value = coords.latitude;
+          lng.value = coords.longitude;
+          return { coords };
+        },
+        (err) => {
+          if (err.message) {
+            throw new Error(err.message);
+          }
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function getCoords() {
+    try {
+      const results = await Geolocation.getCurrentPosition();
+
+      lat.value = results.coords.latitude;
+      lng.value = results.coords.longitude;
+
+      return { coords: results.coords };
+    } catch (error: any) {
+      console.log(error);
+      router.push("/no-gps");
+    }
+  }
+
+  async function watchCoords(): Promise<void> {
+    try {
+      if (watchingCoords.value) {
+        await Geolocation.watchPosition({}, (results) => {
+          lat.value = results?.coords.latitude as number;
+          lng.value = results?.coords.longitude as number;
+        });
+        console.log("watch coords enabled");
+
+        return;
+      }
+
+      console.log("watch coords disabled");
+    } catch (error) {
+      router.push("/no-gps");
+    }
+  }
+
+  async function changeCoords(coords: {
+    lat: number;
+    lng: number;
+  }): Promise<void> {
+    watchingCoords.value = false;
+    lat.value = coords.lat;
+    lng.value = coords.lng;
+  }
+
+  const coords = computed(() => {
+    return { lat: lat.value, lng: lng.value };
+  });
+
+  return {
+    coords,
+    getCoords,
+    watchCoords,
+    changeCoords,
+    getCoordsWithNavigator,
+  };
+});
